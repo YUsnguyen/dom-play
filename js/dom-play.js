beeefbc2aea98feb3f8083d641a957d1a1fd02c2
YUsnguyen <snguyen000@sea.yearup.org>
2022-07-19 12:28:54 -0700

    update code

---
+++ js/dom-play.js
@@ -0,0 +1,37 @@
+// alert("Hello!");
+
+// let actors = document.querySelectorAll(".hamlet");
+
+// console.log(actors);
+ 
+function myAlert(){
+    alert("I'm clicked!");
+}
+
+let spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click", myAlert);
+
+}
+
+
+
+
+
+
+function highlight(el){
+
+    if(el.style.backgroundColor == "yellow"){
+
+        el.style.backgroundColor ='white';
+
+    }
+    else{
+        el.style.backgroundColor='yellow';
+
+    }
+    
+}
