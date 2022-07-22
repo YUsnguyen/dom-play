44d68d13f2f3d7a54825d058b7e2f678c8a76f32
YUsnguyen <snguyen000@sea.yearup.org>
2022-07-21 23:04:18 -0700

    submission

--- js/dom-play.js
+++ js/dom-play.js
@@ -12,14 +12,31 @@ let spans = document.querySelectorAll("#play span");
 
 console.log(spans);
 
+//loads the click as the page is loading
 for(const mySpan of spans){
-    mySpan.addEventListener("click", myAlert);
+    mySpan.addEventListener("click", function(ev){ 
 
+        highlightActor(mySpan.dataset.actor);
+
+    });
+//     alert(mySpan.dataset.actor);
 }
 
+function highlightActor(actor){
 
+    for(const mySpan of spans){
+       if(actor == mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor='yellow';
 
+       }else{//not current actor, remove highlight 
+            mySpan.style.backgroundColor='white';
 
+       }
+    
+        }
+
+    // alert("The current actor is: " + actor);
+}
 
 
 function highlight(el){
@@ -27,11 +44,9 @@ function highlight(el){
     if(el.style.backgroundColor == "yellow"){
 
         el.style.backgroundColor ='white';
-
     }
     else{
-        el.style.backgroundColor='yellow';
-
+        el.style.backgroundColor ='yellow';
     }
     
 }
